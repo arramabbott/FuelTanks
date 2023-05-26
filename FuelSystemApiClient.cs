83edeee15702249731abffa0f8a7b8ea471fb82c
Arram Abbott <arram.abbott@gmail.com>
2023-05-26 10:22:26 -0400

    git init push

---
+++ FuelSystemApiClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+public class FuelSystemApiClient
+{
+    private HttpClient client;
+
+    public FuelSystemApiClient()
+    {
+        client = new HttpClient();
+        client.BaseAddress = new Uri("http://localhost:5000/"); // Replace with your API base URL
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public async Task<FuelSystemName> GetFuelSystemName(string id)
+    {
+        HttpResponseMessage response = await client.GetAsync($"api/fuelsystemnames/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            FuelSystemName fuelSystemName = await response.Content.ReadAsAsync<FuelSystemName>();
+            return fuelSystemName;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public async Task<FuelSystemName> CreateFuelSystemName(FuelSystemName fuelSystemName)
+    {
+        HttpResponseMessage response = await client.PostAsJsonAsync("api/fuelsystemnames", fuelSystemName);
+        response.EnsureSuccessStatusCode();
+
+        FuelSystemName createdFuelSystemName = await response.Content.ReadAsAsync<FuelSystemName>();
+        return createdFuelSystemName;
+    }
+
+    public async Task<FuelSystemName> UpdateFuelSystemName(string id, FuelSystemName fuelSystemName)
+    {
+        HttpResponseMessage response = await client.PutAsJsonAsync($"api/fuelsystemnames/{id}", fuelSystemName);
+        response.EnsureSuccessStatusCode();
+
+        FuelSystemName updatedFuelSystemName = await response.Content.ReadAsAsync<FuelSystemName>();
+        return updatedFuelSystemName;
+    }
+
+    public async Task<bool> DeleteFuelSystemName(string id)
+    {
+        HttpResponseMessage response = await client.DeleteAsync($"api/fuelsystemnames/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System;
+
+/*
+ * Class Data for FuelSystemName, Consumable, UnitOfMeasure
+ */
+public class FuelSystemName
+{
+    internal string ID;
+    public string Name { get; set; }
+    public List<String> FuelTypes { get; set; }
+    public string TankCapacity { get; set; }
+}
+
+public class Consumable
+{
+    public int ID { get; set; }
+    public string Description { get; set; }
+}
+
+public class UnitOfMeasure
+{
+    public int ID { get; set; }
+    public string Description { get; set; }
+}
+
+/*
+ * Main application for creating data in the console
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<FuelSystemName> fuelTrucks = new List<FuelSystemName>();
+        FuelSystemName truck1 = new FuelSystemName
+        {
+            Name = "FuelTruck",
+            ID = "001",
+            FuelTypes = new List<string> { "Diesel"},
+            TankCapacity = "10,000 Gallons"
+
+        };
+        fuelTrucks.Add(truck1);
+
+        FuelSystemName truck2 = new FuelSystemName
+        {
+            Name = "FuelTruck",
+            ID = "002",
+            FuelTypes = new List<string> { "Gasoline" },
+            TankCapacity = "5,000 Liters"
+        };
+        fuelTrucks.Add(truck2);
+
+        FuelSystemName truck3 = new FuelSystemName
+        {
+            Name = "FuelTruck",
+            ID = "003",
+            FuelTypes = new List<string> { "Gasoline" },
+            TankCapacity = "5,000 Liters"
+        };
+        fuelTrucks.Add(truck2);
+
+        Console.WriteLine("Fuel System Name, Tank Capacity, Fuel Info");
+        foreach (FuelSystemName truck in fuelTrucks)
+        {
+            Console.WriteLine(truck.Name + truck.ID + "," + truck.TankCapacity + "," + string.Join(", ", truck.FuelTypes));
+        }
+        Console.ReadLine();
+    }
+}
+
